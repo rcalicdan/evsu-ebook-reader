a172af7675099db5663be98e7e4ad8fd64f9ea6d
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2026-02-11 11:46:52 +0800

    updated code

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -8,25 +8,23 @@
 
 <div class="w-full">
     <div class="relative">
-        @if($icon)
+        @if ($icon)
             <div class="absolute left-3 top-3 text-gray-400">
                 {!! $icon !!}
             </div>
         @endif
-        
-        <input 
-            type="{{ $type }}" 
-            @if($name) name="{{ $name }}" @endif
+
+        <input type="{{ $type }}" @if ($name) name="{{ $name }}" @endif
             placeholder="{{ $placeholder }}"
             {{ $attributes->merge([
-                'class' => 'w-full pr-4 py-2.5 bg-gray-50 border border-gray-200 rounded-xl text-sm focus:ring-2 focus:ring-university-red/20 focus:border-university-red transition-all outline-none ' . 
-                          ($icon ? 'pl-10' : 'pl-4') . 
-                          ($error ? ' border-red-300 focus:border-red-500 focus:ring-red-200' : '')
-            ]) }}
-        >
+                'class' =>
+                    'w-full pr-4 py-2.5 bg-gray-50 border border-gray-200 rounded-xl text-sm focus:ring-2 focus:ring-university-red/20 focus:border-university-red transition-all outline-none ' .
+                    ($icon ? 'pl-10' : 'pl-4') .
+                    ($error ? ' border-red-300 focus:border-red-500 focus:ring-red-200' : ''),
+            ]) }}>
     </div>
-    
-    @if($error)
+
+    @if ($error)
         <p class="text-red-500 text-xs mt-1.5">{{ $error }}</p>
     @endif
 </div>
