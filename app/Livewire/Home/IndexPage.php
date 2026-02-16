230e263c68e3b2b896325b50af342ea515094bb5
anthonymacs <157227172+anthonymacs@users.noreply.github.com>
2026-02-16 08:05:56 +0700

    updated homepage index

---
+++ app/Livewire/Home/IndexPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Home;
+
+use Livewire\Component;
+
+class IndexPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.home.index-page');
+    }
+}
