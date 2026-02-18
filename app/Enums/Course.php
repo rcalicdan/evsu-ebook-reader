101d6cd95562bfb1a40b977e0ae030832a4c7aa1
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2026-02-18 16:19:25 +0800

    updated login register page

---
+++ app/Enums/Course.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Enums;
+
+enum Course: string
+{
+    case BSChE  = 'BSChE';
+    case BSCE   = 'BSCE';
+    case BSEE   = 'BSEE';
+    case BSECE  = 'BSECE';
+    case BSGE   = 'BSGE';
+    case BSIE   = 'BSIE';
+    case BSIT   = 'BSIT';
+    case BSME   = 'BSME';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::BSChE  => 'BS Chemical Engineering (BSChE)',
+            self::BSCE   => 'BS Civil Engineering (BSCE)',
+            self::BSEE   => 'BS Electrical Engineering (BSEE)',
+            self::BSECE  => 'BS Electronics & Communications Engineering (BSECE)',
+            self::BSGE   => 'BS Geodetic Engineering (BSGE)',
+            self::BSIE   => 'BS Industrial Engineering (BSIE)',
+            self::BSIT   => 'BS Information Technology (BSIT)',
+            self::BSME   => 'BS Mechanical Engineering (BSME)',
+        };
+    }
+}
--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Auth;
 
+use App\Enums\Course;
+use Illuminate\Validation\Rule;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -10,16 +12,22 @@ class Register extends Component
 {
     public string $name = '';
     public string $email = '';
+    public string $course = '';
     public string $password = '';
     public string $password_confirmation = '';
 
     public function register(): void
     {
-        // Add registration logic later
+        $this->validate([
+            'course' => ['required', Rule::enum(Course::class)],
+            // ...other rules
+        ]);
     }
 
     public function render()
     {
-        return view('livewire.auth.register');
+        return view('livewire.auth.register', [
+            'courses' => Course::cases(),
+        ]);
     }
 }
